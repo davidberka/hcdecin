cd3b636e6b5ad4e760551e27337adc31763ebb9e
David Berka <davidberka2005@gmail.com>
2021-11-05 21:54:44 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './dist/**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "main-blue": "#023878",
+        "main-red": "#E30613",
+      },
+      fontSize: {
+        "h1": "3.5rem",
+        "h3": "2.5rem",
+      },
+      fontFamily: {
+        "body": ['"Barlow"', "sans-serif"],
+      },
+      height: {
+        "22": "5.5rem",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
